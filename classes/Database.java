8b51377d28bef40710e07fbb938c4b06a37cab56
JYuniform64 <jiny2048@gmail.com>
2021-06-11 23:57:18 +0800

    file: Authentication.java, Database.java

---
+++ classes/Database.java
@@ -0,0 +1,22 @@
+public interface Database {
+    // Get uid corresponds to a certain username.
+    public long getUid(String username);
+
+    // Get username corresponds to a certain uid.
+    public String getUsername(long uid);
+
+    // Get uid by a **valid** email.
+    // Should be used by 'email' module.
+    public long getUidByEmail(String email);
+
+    // Get uid by a **valid** token.
+    public long getUidByToken(long token);
+
+    // Judge whether a username already exists.
+    // Should be used in **SignUp**.
+    public boolean usernameExist(String username);
+
+    // Judge whether a email already exists.
+    // Should be used in **SignUp**.
+    public boolean emailExist(String email);
+}
