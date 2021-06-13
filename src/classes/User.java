6e39e29605b52a1ade599cac6007448cb7eb4e71
JYuniform64 <jiny2048@gmail.com>
2021-06-13 22:50:17 +0800

    file: 'User Status Management' module'

---
+++ src/classes/User.java
@@ -0,0 +1,20 @@
+package com.group.jsp;
+
+public interface User {
+    // Sign-in.
+    // Return uid(greater than 0) on success, -SYS_ERR on system error, -USR_ERR on
+    // user error.
+    public long signIn(String email_or_username, String password, boolean is_email);
+
+    // Register a user.
+    // Return 1 on success, -SYS_ERR on system error, -USR_ERR on user error.
+    public int signUp(String username, String password, String email);
+
+    // Close an account given its uid.
+    public boolean destoryAccount(long uid);
+
+    // Excute precudure corresponding to forget password.
+    // Return 1 on successfully sending email & register a special token for
+    // resetting password, -SYS_ERR on system error, -USR_ERR on user error.
+    public int forgetPasswdProcedure(String email);
+}
